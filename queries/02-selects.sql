a23245626b1c6fa49e1c1384609af05c1b8c32c9
Degrava <juhcdgomes@gmail.com>
2025-09-15 10:53:11 -0300

    Feat: Finaliza selects

--- queries/02-selects.sql
+++ queries/02-selects.sql
@@ -1,3 +1,9 @@
 SELECT * FROM artistas;
 
 SELECT COUNT(*)As total_artistas FROM artistas;
+
+SELECT nome, musica_mais_famosa FROM artistas;
+
+SELECT nome, musica_mais_famosa FROM artistas WHERE pais_origem = 'Brasil';
+
+SELECT nome, musica_mais_famosa FROM artistas WHERE id = 3;
