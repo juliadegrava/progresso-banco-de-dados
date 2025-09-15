3e38ca74ff9e19ac7d0dae61592593ab8ea01278
Degrava <juhcdgomes@gmail.com>
2025-09-15 11:43:32 -0300

    Feat: Adiciona tabela de animas

--- queries/01-insert-artists.sql
+++ queries/01-insert-artists.sql
@@ -15,3 +15,25 @@ VALUES
 ('System of A Down', 'Banda', 4, 'Metal', 'Estragos Unidos', 1994, 'Chop Suey!', true),
 ('Twice', 'Girlgroup', 9, 'KPOP', 'Coreia do Sul', '2015', 'What is Love?', true),
 ('Taylor Swift', 'Solista', 1, 'Pop, Country', 'EUA', '2006', 'Shake it Off', true);
+
+CREATE TABLE animais (
+    id SERIAL PRIMARY KEY,
+    nome VARCHAR(50),
+    tempo_medio_vida VARCHAR(8),
+    cor_padrao VARCHAR(50),
+    quantidade_patas INT,
+    extinto BOOLEAN DEFAULT FALSE
+)
+
+INSERT INTO animais (nome, tempo_medio_vida, cor_padrao, quantidade_patas, extinto );
+VALUES
+('aranha marrom', '1 ano', 'marrom', 8, false),
+('ararinha azul', '30 anos', 'azul', 2, true),
+('tigre dente-de-sabre', '40 anos', 'bege', 4, true),
+('tartaruga', '100 anos', 'verde', 4, false),
+('dodo', '20 anos', 'cinza', 2, true),
+('furao', '7 anos', 'variada', 4, false),
+('vaca', '15 anos', 'preto e branco, marrom', 4, false),
+('mamute', '60 anos', 'marrom', 4, true),
+('mico-leao-dourado', '10 anos', 'laranja', 4, false),
+('triceratops', '25 anos', 'marrom, acinzentado', 4, true);
--- queries/02-selects.sql
+++ queries/02-selects.sql
@@ -7,3 +7,12 @@ SELECT nome, musica_mais_famosa FROM artistas;
 SELECT nome, musica_mais_famosa FROM artistas WHERE pais_origem = 'Brasil';
 
 SELECT nome, musica_mais_famosa FROM artistas WHERE id = 3;
+
+
+SELECT * FROM animais;
+
+SELECT COUNT(*)As total_artistas FROM animais;
+
+SELECT nome, extinto FROM animais;
+
+SELECT nome, extinto FROM animais WHERE extinto = true;
