bde3efec7b2a04bb608b6e597ec59f51e2a9d7c8
Degrava <juhcdgomes@gmail.com>
2025-09-22 14:42:55 -0300

    feat: comandos iniciais de update

---
+++ queries/03-update.sql
@@ -0,0 +1,11 @@
+SELECT * FROM usuarios;
+
+UPDATE usuarios SET username = 'ProGamer2025'
+WHERE username = 'NoobMaster';
+
+UPDATE SET pontos = pontos + 1000;
+
+SELECT * FROM usuarios;
+
+UPDATE usuarios SET email = 'main@gmail.com'
+WHERE email = 'master@gmail.com';
