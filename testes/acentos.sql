2ffaa5e9951c8b5a656e4d1b7d32f1c9b0fa1a70
julia <juhcdgomes@gmail.com>
2025-09-29 11:43:29 -0300

    feat: cria tabela de testes com dados de exemplo

---
+++ testes/acentos.sql
@@ -0,0 +1,15 @@
+CREATE TABLE testes (
+    id SERIAL PRIMARY KEY,
+    nome VARCHAR(100) NOT NULL
+);
+
+INSERT INTO testes (nome)
+VALUES
+('João'),
+('Júlia'),
+('Ângelo'),
+('Letícia'),
+('Stéfanny'),
+('Vinícius'),
+('Björk'),
+('François');
